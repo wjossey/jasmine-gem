0523f64d6f4ef8fd8ee5549c2872188f6b08a7b0
Davis W. Frank <pair+dwfrank@pivotallabs.com>
2010-09-24 10:17:40 -0700

    Fix to the runner so jasmine:ci works

--- generators/jasmine/templates/spec/javascripts/support/jasmine_runner.rb
+++ generators/jasmine/templates/spec/javascripts/support/jasmine_runner.rb
@@ -1,7 +1,10 @@
 $:.unshift(ENV['JASMINE_GEM_PATH']) if ENV['JASMINE_GEM_PATH'] # for gem testing purposes
 
+ENV["JASMINE_BROWSER"] = "safari"
+
 require 'rubygems'
 require 'jasmine'
+require 'rspec'
 jasmine_config_overrides = File.expand_path(File.join(File.dirname(__FILE__), 'jasmine_config.rb'))
 require jasmine_config_overrides if File.exists?(jasmine_config_overrides)
 
@@ -10,10 +13,8 @@ spec_builder = Jasmine::SpecBuilder.new(jasmine_config)
 
 should_stop = false
 
-Spec::Runner.configure do |config|
-  config.after(:suite) do
+RSpec.configuration.after(:suite) do
     spec_builder.stop if should_stop
-  end
 end
 
 spec_builder.start
